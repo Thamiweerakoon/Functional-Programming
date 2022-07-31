f8e517c0c4cf8e2f96fe04c99efc03e37157117e
Thamiweerakoon <108712564+Thamiweerakoon@users.noreply.github.com>
2022-07-31 20:38:34 +0530

    Add files via upload

---
+++ T4/Q1.scala
@@ -0,0 +1,16 @@
+import scala.io.StdIn.readInt
+
+object Q1 extends App{
+
+    def calintereset(deposit:Double):Double=deposit match{
+        case x if x<0 => 0;
+        case x if x<20000 => x*0.02;
+        case x if x<200000 => x*0.04;
+        case x if x<2000000 => x*0.035;
+        case x if x>2000000 => x*0.065;
+    }
+
+    print("Enter deposit amount: ");
+    var x=readInt();
+    print("Interest: "+calintereset(x));
+}
